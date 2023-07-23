b29a49202b6a585ccfabaf51714fb4a31d600a0e
hushaim <hushaim52@gmail.com>
2023-07-23 01:24:50 +0000

    new files

--- class4-statefile/main.tf
+++ class4-statefile/main.tf
@@ -3,8 +3,8 @@ resource "aws_key_pair" "deployer" {
   key_name   = "deployer176"
   public_key = file("~/.ssh/id_rsa.pub")
   tags = {
-    team ="devops"
-    env ="dev"
+    team ="dev7ops"
+    env ="de3v"
   }
 
 }
